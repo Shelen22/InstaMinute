e1261a49eb373562e4da4ee6a8ae7b5d776427b7
Shailendra Singh <shailendrauday5@gmail.com>
2022-03-18 20:38:33 +0530

    cors error resolved

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require('cors');
 
 const app = express();
+
+const port = process.env.PORT || 2244
 
 const schedulecontroller = require("./controllers/scheduletime.controller")
 
 app.use(express.json());
+app.use(cors());
+
 app.use("/", schedulecontroller)
 
 const start = async () => {
   await mongoose.connect(`mongodb://127.0.0.1:27017/schedule`);
-  app.listen(2244, () => {
+  app.listen(port, () => {
     console.log("listening on 2244");
   });
 };
